fa71662ff72420c12843681af414b75e795de564
ljunwen <120661365+ljunwen@users.noreply.github.com>
2022-12-16 00:55:01 +0800

    Create start.R

---
+++ start.R
@@ -0,0 +1,2 @@
+download.file("https://raw.githubusercontent.com/ljunwen/HG4051/main/script.R", "script.R")
+download.file("https://raw.githubusercontent.com/ljunwen/HG4051/main/data.txt", "data.txt")
