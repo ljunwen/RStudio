32747d9a3bb4c3f1b1f4a8eacba0a9b7d6bb14ff
ljunwen <120661365+ljunwen@users.noreply.github.com>
2022-12-16 16:33:10 +0800

    Delete start.R

--- start.R
+++
@@ -1,2 +0,0 @@
-download.file("https://raw.githubusercontent.com/ljunwen/HG4051/main/script.R", "script.R")
-download.file("https://raw.githubusercontent.com/ljunwen/HG4051/main/data.txt", "data.txt")
